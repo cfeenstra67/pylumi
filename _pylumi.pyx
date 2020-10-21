711ac3843e95d1ceccac60636556e88684b7c891
cfeenstra67 <cameron.l.feenstra@gmail.com>
2020-10-21 01:31:29 -0400

    formatting

--- _pylumi.pyx
+++ _pylumi.pyx
@@ -5,328 +5,328 @@ from libc.stdlib cimport malloc, free
 
 cdef extern from "libpylumigo.h":
 
-	ctypedef signed char GoInt8
-	ctypedef unsigned char GoUint8
-	ctypedef short GoInt16
-	ctypedef unsigned short GoUint16
-	ctypedef int GoInt32
-	ctypedef unsigned int GoUint32
-	ctypedef long long GoInt64
-	ctypedef unsigned long long GoUint64
-	ctypedef GoInt64 GoInt
-	ctypedef GoUint64 GoUint
-	ctypedef float GoFloat32
-	ctypedef double GoFloat64
+    ctypedef signed char GoInt8
+    ctypedef unsigned char GoUint8
+    ctypedef short GoInt16
+    ctypedef unsigned short GoUint16
+    ctypedef int GoInt32
+    ctypedef unsigned int GoUint32
+    ctypedef long long GoInt64
+    ctypedef unsigned long long GoUint64
+    ctypedef GoInt64 GoInt
+    ctypedef GoUint64 GoUint
+    ctypedef float GoFloat32
+    ctypedef double GoFloat64
 
-	struct ContextSetup_return:
-		GoInt r0
-		char* r1
+    struct ContextSetup_return:
+        GoInt r0
+        char* r1
 
-	ContextSetup_return ContextSetup(char* name, char* cwd)
+    ContextSetup_return ContextSetup(char* name, char* cwd)
 
-	struct ContextTeardown_return:
-		GoInt r0
-		char* r1
+    struct ContextTeardown_return:
+        GoInt r0
+        char* r1
 
-	ContextTeardown_return ContextTeardown(char* name)
+    ContextTeardown_return ContextTeardown(char* name)
 
-	struct ContextListPlugins_return:
-		GoInt r0
-		char** r1
-		GoInt r2
-		char* r3
+    struct ContextListPlugins_return:
+        GoInt r0
+        char** r1
+        GoInt r2
+        char* r3
 
-	ContextListPlugins_return ContextListPlugins(char* name)
+    ContextListPlugins_return ContextListPlugins(char* name)
 
-	struct ProviderTeardown_return:
-		GoInt r0
-		char* r1
+    struct ProviderTeardown_return:
+        GoInt r0
+        char* r1
 
-	ProviderTeardown_return ProviderTeardown(char* ctx, char* provider)
+    ProviderTeardown_return ProviderTeardown(char* ctx, char* provider)
 
-	struct ProviderGetSchema_return:
-		GoInt r0
-		char* r1
-		char* r2
+    struct ProviderGetSchema_return:
+        GoInt r0
+        char* r1
+        char* r2
 
-	ProviderGetSchema_return ProviderGetSchema(char* ctxName, char* name, GoInt version)
+    ProviderGetSchema_return ProviderGetSchema(char* ctxName, char* name, GoInt version)
 
-	struct ProviderCheckConfig_return:
-		GoInt r0
-		char* r1
-		char* r2
-		char* r3
+    struct ProviderCheckConfig_return:
+        GoInt r0
+        char* r1
+        char* r2
+        char* r3
 
-	ProviderCheckConfig_return ProviderCheckConfig(char* ctx, char* provider, char* urn, char* olds, char* news, GoUint8 allowUnknowns)
+    ProviderCheckConfig_return ProviderCheckConfig(char* ctx, char* provider, char* urn, char* olds, char* news, GoUint8 allowUnknowns)
 
-	struct ProviderDiffConfig_return:
-		GoInt r0
-		char* r1
-		char* r2
+    struct ProviderDiffConfig_return:
+        GoInt r0
+        char* r1
+        char* r2
 
-	ProviderDiffConfig_return ProviderDiffConfig(char* ctx, char* provider, char* urn, char* olds, char* news, GoUint8 allowUnknowns, char** ignoreChanges, GoInt nIgnoreChanges)
+    ProviderDiffConfig_return ProviderDiffConfig(char* ctx, char* provider, char* urn, char* olds, char* news, GoUint8 allowUnknowns, char** ignoreChanges, GoInt nIgnoreChanges)
 
-	struct ProviderConfigure_return:
-		GoInt r0
-		char* r1
+    struct ProviderConfigure_return:
+        GoInt r0
+        char* r1
 
-	ProviderConfigure_return ProviderConfigure(char* ctx, char* provider, char* inputs)
+    ProviderConfigure_return ProviderConfigure(char* ctx, char* provider, char* inputs)
 
-	struct ProviderCheck_return:
-		GoInt r0
-		char* r1
-		char* r2
-		char* r3
+    struct ProviderCheck_return:
+        GoInt r0
+        char* r1
+        char* r2
+        char* r3
 
-	ProviderCheck_return ProviderCheck(char* ctx, char* provider, char* urn, char* olds, char* news, GoUint8 allowUnknowns)
+    ProviderCheck_return ProviderCheck(char* ctx, char* provider, char* urn, char* olds, char* news, GoUint8 allowUnknowns)
 
-	struct ProviderDiff_return:
-		GoInt r0
-		char* r1
-		char* r2
+    struct ProviderDiff_return:
+        GoInt r0
+        char* r1
+        char* r2
 
-	ProviderDiff_return ProviderDiff(char* ctx, char* provider, char* urn, char* id, char* olds, char* news, GoUint8 allowUnknowns, char** ignoreChanges, GoInt nIgnoreChanges)
+    ProviderDiff_return ProviderDiff(char* ctx, char* provider, char* urn, char* id, char* olds, char* news, GoUint8 allowUnknowns, char** ignoreChanges, GoInt nIgnoreChanges)
 
-	struct ProviderCreate_return:
-		GoInt r0
-		char* r1
-		char* r2
+    struct ProviderCreate_return:
+        GoInt r0
+        char* r1
+        char* r2
 
-	ProviderCreate_return ProviderCreate(char* ctx, char* provider, char* urn, char* news, GoFloat64 timeout, GoUint8 preview)
+    ProviderCreate_return ProviderCreate(char* ctx, char* provider, char* urn, char* news, GoFloat64 timeout, GoUint8 preview)
 
-	struct ProviderRead_return:
-		GoInt r0
-		char* r1
-		char* r2
+    struct ProviderRead_return:
+        GoInt r0
+        char* r1
+        char* r2
 
-	ProviderRead_return ProviderRead(char* ctx, char* provider, char* urn, char* id, char* inputs, char* state)
+    ProviderRead_return ProviderRead(char* ctx, char* provider, char* urn, char* id, char* inputs, char* state)
 
-	struct ProviderUpdate_return:
-		GoInt r0
-		char* r1
-		char* r2
+    struct ProviderUpdate_return:
+        GoInt r0
+        char* r1
+        char* r2
 
-	ProviderUpdate_return ProviderUpdate(char* ctx, char* provider, char* urn, char* id, char* olds, char* news, GoFloat64 timeout, char** ignoreChanges, GoInt nIgnoreChanges, GoUint8 preview)
+    ProviderUpdate_return ProviderUpdate(char* ctx, char* provider, char* urn, char* id, char* olds, char* news, GoFloat64 timeout, char** ignoreChanges, GoInt nIgnoreChanges, GoUint8 preview)
 
-	struct ProviderDelete_return:
-		GoInt r0
-		GoInt r1
-		char* r2
+    struct ProviderDelete_return:
+        GoInt r0
+        GoInt r1
+        char* r2
 
-	ProviderDelete_return ProviderDelete(char* ctx, char* provider, char* urn, char* id, char* news, GoFloat64 timeout)
+    ProviderDelete_return ProviderDelete(char* ctx, char* provider, char* urn, char* id, char* news, GoFloat64 timeout)
 
 
 # Helper functions
 cdef bytes _bytes(s):
-	"""
-	Coerce text input to bytes
-	"""
-	if type(s) is bytes:
-		return s
-	if isinstance(s, str):
-		return s.encode()
-	raise TypeError(f'Invalid bytes value: {s}.')
+    """
+    Coerce text input to bytes
+    """
+    if type(s) is bytes:
+        return s
+    if isinstance(s, str):
+        return s.encode()
+    raise TypeError(f'Invalid bytes value: {s}.')
 
 
 cdef str _str(s):
-	"""
-	Coerce text input to bytes
-	"""
-	if isinstance(s, str):
-		return s
-	if isinstance(s, bytes):
-		return s.decode()
-	raise TypeError(f'Invalid str value: {s}.')
+    """
+    Coerce text input to bytes
+    """
+    if isinstance(s, str):
+        return s
+    if isinstance(s, bytes):
+        return s.decode()
+    raise TypeError(f'Invalid str value: {s}.')
 
 
 cdef char ** to_cstring_array(list_str):
-	cdef char **ret = <char **>malloc(len(list_str) * sizeof(char *))
-	for i in xrange(len(list_str)):
-		as_bytes = list_str[i].encode()
-		ret[i] = as_bytes
-	return ret
+    cdef char **ret = <char **>malloc(len(list_str) * sizeof(char *))
+    for i in xrange(len(list_str)):
+        as_bytes = list_str[i].encode()
+        ret[i] = as_bytes
+    return ret
 
 
 # Context methods
 
 def context_setup(str ctxName, str cwd):
-	res = ContextSetup(_bytes(ctxName), _bytes(cwd))
-	if res.r0 == 0:
-		return None
-	raise ContextError(res.r0, _str(res.r1))
+    res = ContextSetup(_bytes(ctxName), _bytes(cwd))
+    if res.r0 == 0:
+        return None
+    raise ContextError(res.r0, _str(res.r1))
 
 
 def context_teardown(str ctxName):
-	res = ContextTeardown(_bytes(ctxName))
-	if res.r0 == 0:
-		return None
-	raise ContextError(res.r0, _str(res.r1))
+    res = ContextTeardown(_bytes(ctxName))
+    if res.r0 == 0:
+        return None
+    raise ContextError(res.r0, _str(res.r1))
 
 
 def context_list_plugins(str ctxName):
-	res = ContextListPlugins(_bytes(ctxName))
-	if res.r0 == 0:
-		return [x.decode() for x in res.r1[:res.r2]]
-	raise ContextError(res.r0, _str(res.r3))
+    res = ContextListPlugins(_bytes(ctxName))
+    if res.r0 == 0:
+        return [x.decode() for x in res.r1[:res.r2]]
+    raise ContextError(res.r0, _str(res.r3))
 
 
 # Provider methods
 
 def provider_teardown(str ctx, str provider):
-	res = ProviderTeardown(_bytes(ctx), _bytes(provider))
-	if res.r0 == 0:
-		return None
-	raise ProviderError(res.r0, _str(res.r1))
+    res = ProviderTeardown(_bytes(ctx), _bytes(provider))
+    if res.r0 == 0:
+        return None
+    raise ProviderError(res.r0, _str(res.r1))
 
 
 def provider_get_schema(str ctxName, str name, int version):
-	res = ProviderGetSchema(_bytes(ctxName), _bytes(name), version)
-	if res.r0 == 0:
-		return res.r1
-	raise ProviderError(res.r0, _str(res.r2))
+    res = ProviderGetSchema(_bytes(ctxName), _bytes(name), version)
+    if res.r0 == 0:
+        return res.r1
+    raise ProviderError(res.r0, _str(res.r2))
 
 
 def provider_check_config(str ctx, str provider, str urn, olds, news, allow_unknowns=False):
-	olds_encoded = json.dumps(olds).encode()
-	news_encoded = json.dumps(news).encode()
-	res = ProviderCheckConfig(
-		_bytes(ctx), _bytes(provider), _bytes(urn),
-		olds_encoded, news_encoded, allow_unknowns
-	)
-	if res.r0 == 0:
-		props_decoded = json.loads(_bytes(res.r1))
-		failures_decoded = json.loads(_bytes(res.r2))
-		return props_decoded, failures_decoded
-	raise ProviderError(res.r0, _str(res.r3))
+    olds_encoded = json.dumps(olds).encode()
+    news_encoded = json.dumps(news).encode()
+    res = ProviderCheckConfig(
+        _bytes(ctx), _bytes(provider), _bytes(urn),
+        olds_encoded, news_encoded, allow_unknowns
+    )
+    if res.r0 == 0:
+        props_decoded = json.loads(_bytes(res.r1))
+        failures_decoded = json.loads(_bytes(res.r2))
+        return props_decoded, failures_decoded
+    raise ProviderError(res.r0, _str(res.r3))
 
 
 def provider_diff_config(str ctx, str provider, str urn, olds, news, allow_unknowns=False, ignore_changes=()):
-	olds_encoded = json.dumps(olds).encode()
-	news_encoded = json.dumps(news).encode()
-	res = ProviderDiffConfig(
-		_bytes(ctx), _bytes(provider), _bytes(urn),
-		olds_encoded, news_encoded,
-		allow_unknowns, to_cstring_array(ignore_changes), len(ignore_changes)
-	)
-	if res.r0 == 0:
-		out_decoded = json.loads(_bytes(res.r1))
-		return out_decoded
-	raise ProviderError(res.r0, _str(res.r2))
+    olds_encoded = json.dumps(olds).encode()
+    news_encoded = json.dumps(news).encode()
+    res = ProviderDiffConfig(
+        _bytes(ctx), _bytes(provider), _bytes(urn),
+        olds_encoded, news_encoded,
+        allow_unknowns, to_cstring_array(ignore_changes), len(ignore_changes)
+    )
+    if res.r0 == 0:
+        out_decoded = json.loads(_bytes(res.r1))
+        return out_decoded
+    raise ProviderError(res.r0, _str(res.r2))
 
 
 def provider_configure(str ctx, str provider, inputs):
-	inputs_encoded = json.dumps(inputs).encode()
-	res = ProviderConfigure(_bytes(ctx), _bytes(provider), inputs_encoded)
-	if res.r0 == 0:
-		return None
-	raise ProviderError(res.r0, _str(res.r1))
+    inputs_encoded = json.dumps(inputs).encode()
+    res = ProviderConfigure(_bytes(ctx), _bytes(provider), inputs_encoded)
+    if res.r0 == 0:
+        return None
+    raise ProviderError(res.r0, _str(res.r1))
 
 
 def provider_check(str ctx, str provider, str urn, olds, news, allow_unknowns=False):
-	olds_encoded = json.dumps(olds).encode()
-	news_encoded = json.dumps(news).encode()
-	res = ProviderCheck(
-		_bytes(ctx), _bytes(provider), _bytes(urn),
-		olds_encoded, news_encoded, allow_unknowns
-	)
-	if res.r0 == 0:
-		props = json.loads(_bytes(res.r1))
-		failures = json.loads(_bytes(res.r2))
-		return props, failures
-	raise ProviderError(res.r0, _str(res.r3))
+    olds_encoded = json.dumps(olds).encode()
+    news_encoded = json.dumps(news).encode()
+    res = ProviderCheck(
+        _bytes(ctx), _bytes(provider), _bytes(urn),
+        olds_encoded, news_encoded, allow_unknowns
+    )
+    if res.r0 == 0:
+        props = json.loads(_bytes(res.r1))
+        failures = json.loads(_bytes(res.r2))
+        return props, failures
+    raise ProviderError(res.r0, _str(res.r3))
 
 
 def provider_diff(str ctx, str provider, str urn, str id, olds, news, allow_unknowns=False, ignore_changes=()):
-	olds_encoded = json.dumps(olds).encode()
-	news_encoded = json.dumps(news).encode()
-	res = ProviderDiff(
-		_bytes(ctx), _bytes(provider), _bytes(urn), _bytes(id),
-		olds_encoded, news_encoded,
-		allow_unknowns, to_cstring_array(ignore_changes), len(ignore_changes)
-	)
-	if res.r0 == 0:
-		out_decoded = json.loads(_bytes(res.r1))
-		return out_decoded
-	raise ProviderError(res.r0, _str(res.r2))
+    olds_encoded = json.dumps(olds).encode()
+    news_encoded = json.dumps(news).encode()
+    res = ProviderDiff(
+        _bytes(ctx), _bytes(provider), _bytes(urn), _bytes(id),
+        olds_encoded, news_encoded,
+        allow_unknowns, to_cstring_array(ignore_changes), len(ignore_changes)
+    )
+    if res.r0 == 0:
+        out_decoded = json.loads(_bytes(res.r1))
+        return out_decoded
+    raise ProviderError(res.r0, _str(res.r2))
 
 
 def provider_create(str ctx, str provider, str urn, news, int timeout=60, preview=False):
-	news_encoded = json.dumps(news).encode()
-	res = ProviderCreate(
-		_bytes(ctx), _bytes(provider), _bytes(urn),
-		news_encoded, timeout, preview
-	)
-	if res.r0 == 0:
-		out_decoded = json.loads(_bytes(res.r1))
-		return out_decoded
-	raise ProviderError(res.r0, _str(res.r2))
+    news_encoded = json.dumps(news).encode()
+    res = ProviderCreate(
+        _bytes(ctx), _bytes(provider), _bytes(urn),
+        news_encoded, timeout, preview
+    )
+    if res.r0 == 0:
+        out_decoded = json.loads(_bytes(res.r1))
+        return out_decoded
+    raise ProviderError(res.r0, _str(res.r2))
 
 
 def provider_read(str ctx, str provider, str urn, str id, inputs, state):
-	input_encoded = json.dumps(inputs).encode()
-	state_encoded = json.dumps(state).encode()
-	res = ProviderRead(
-		_bytes(ctx), _bytes(provider), _bytes(urn), _bytes(id),
-		input_encoded, state_encoded
-	)
-	if res.r0 == 0:
-		out_decoded = json.loads(_bytes(res.r1))
-		return out_decoded
-	raise ProviderError(res.r0, _str(res.r2))
+    input_encoded = json.dumps(inputs).encode()
+    state_encoded = json.dumps(state).encode()
+    res = ProviderRead(
+        _bytes(ctx), _bytes(provider), _bytes(urn), _bytes(id),
+        input_encoded, state_encoded
+    )
+    if res.r0 == 0:
+        out_decoded = json.loads(_bytes(res.r1))
+        return out_decoded
+    raise ProviderError(res.r0, _str(res.r2))
 
 
 def provider_update(str ctx, str provider, str urn, str id, olds, news, int timeout=60, ignore_changes=(), preview=False):
-	olds_encoded = json.dumps(olds).encode()
-	news_encoded = json.dumps(news).encode()
-	res = ProviderUpdate(
-		_bytes(ctx), _bytes(provider), _bytes(urn), _bytes(id),
-		olds_encoded, news_encoded,
-		timeout, to_cstring_array(ignore_changes), len(ignore_changes), preview
-	)
-	if res.r0 == 0:
-		out_decoded = json.loads(_bytes(res.r1))
-		return out_decoded
-	raise ProviderError(res.r0, _str(res.r2))
+    olds_encoded = json.dumps(olds).encode()
+    news_encoded = json.dumps(news).encode()
+    res = ProviderUpdate(
+        _bytes(ctx), _bytes(provider), _bytes(urn), _bytes(id),
+        olds_encoded, news_encoded,
+        timeout, to_cstring_array(ignore_changes), len(ignore_changes), preview
+    )
+    if res.r0 == 0:
+        out_decoded = json.loads(_bytes(res.r1))
+        return out_decoded
+    raise ProviderError(res.r0, _str(res.r2))
 
 
 def provider_delete(str ctx, str provider, str urn, str id, news, int timeout=60):
-	news_encoded = json.dumps(news).encode()
-	res = ProviderDelete(
-		_bytes(ctx), _bytes(provider), _bytes(urn), _bytes(id),
-		news_encoded, timeout
-	)
-	if res.r0 == 0:
-		return res.r1
-	raise ProviderError(res.r0, _str(res.r2))
+    news_encoded = json.dumps(news).encode()
+    res = ProviderDelete(
+        _bytes(ctx), _bytes(provider), _bytes(urn), _bytes(id),
+        news_encoded, timeout
+    )
+    if res.r0 == 0:
+        return res.r1
+    raise ProviderError(res.r0, _str(res.r2))
 
 
 class PylumiGoError(Exception):
-	"""
-	Errors originating from go within Pylumi
-	"""
+    """
+    Errors originating from go within Pylumi
+    """
 
 
 class ContextError(PylumiGoError):
-	"""
-	Errors from context_ methods
-	"""
-	def __init__(self, int status_code, str message):
-		self.status_code = status_code
-		self.message = message
-		super().__init__(
-			'Error from pulumi context: %s (status code: %d)'
-			% (message, status_code)
-		)
+    """
+    Errors from context_ methods
+    """
+    def __init__(self, int status_code, str message):
+        self.status_code = status_code
+        self.message = message
+        super().__init__(
+            'Error from pulumi context: %s (status code: %d)'
+            % (message, status_code)
+        )
 
 
 class ProviderError(PylumiGoError):
-	"""
-	Errors from provider_ methods
-	"""
-	def __init__(self, int status_code, str message):
-		self.status_code = status_code
-		self.message = message
-		super().__init__(
-			'Error from pulumi provider: %s (status code: %d)'
-			% (message, status_code)
-		)
+    """
+    Errors from provider_ methods
+    """
+    def __init__(self, int status_code, str message):
+        self.status_code = status_code
+        self.message = message
+        super().__init__(
+            'Error from pulumi provider: %s (status code: %d)'
+            % (message, status_code)
+        )
--- go/pylumi/context.go
+++ go/pylumi/context.go
@@ -1,110 +1,110 @@
 package pylumi
 
 import (
-	"fmt"
-	// "path/filepath"
+    "fmt"
+    // "path/filepath"
 
-	"github.com/blang/semver"
+    "github.com/blang/semver"
 
-	// "github.com/pulumi/pulumi/pkg/v2/engine"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
+    // "github.com/pulumi/pulumi/pkg/v2/engine"
+    "github.com/pulumi/pulumi/sdk/v2/go/common/diag"
+    "github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
+    "github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+    "github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 type Context struct {
-	// Info engine.Projinfo
-	PluginCtx *plugin.Context
-	WorkingDirectory string
-	// Main string
-	Sink diag.Sink
-	StatusSink diag.Sink
-	providers map[tokens.Package]*plugin.Provider
+    // Info engine.Projinfo
+    PluginCtx *plugin.Context
+    WorkingDirectory string
+    // Main string
+    Sink diag.Sink
+    StatusSink diag.Sink
+    providers map[tokens.Package]*plugin.Provider
 }
 
 func NewContextFromPath(cwd string, sink, statusSink diag.Sink) (*Context, error) {
-	// projectPath, err := workspace.DetectProjectPathFrom(path)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("error detecting project: %v", err)
-	// }
+    // projectPath, err := workspace.DetectProjectPathFrom(path)
+    // if err != nil {
+    //  return nil, fmt.Errorf("error detecting project: %v", err)
+    // }
 
-	// proj, err := workspace.LoadProject(path)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("error loading project: %v", err)
-	// }
+    // proj, err := workspace.LoadProject(path)
+    // if err != nil {
+    //  return nil, fmt.Errorf("error loading project: %v", err)
+    // }
 
-	// root := filepath.Dir(path)
+    // root := filepath.Dir(path)
 
-	// projInfo := engine.Projinfo{Proj: proj, Root: root}
+    // projInfo := engine.Projinfo{Proj: proj, Root: root}
 
-	// pwd, main, ctx, err := engine.ProjectInfoContext(&projInfo, nil, nil, sink, statusSink, false, nil)
+    // pwd, main, ctx, err := engine.ProjectInfoContext(&projInfo, nil, nil, sink, statusSink, false, nil)
 
-	ctx, err := plugin.NewContext(sink, statusSink, nil, nil, cwd, nil, false, nil)
-	if err != nil {
-		return nil, fmt.Errorf("error obtaining context: %v", err)
-	}
+    ctx, err := plugin.NewContext(sink, statusSink, nil, nil, cwd, nil, false, nil)
+    if err != nil {
+        return nil, fmt.Errorf("error obtaining context: %v", err)
+    }
 
-	newCtx := Context{
-		// Info: projInfo,
-		PluginCtx: ctx,
-		WorkingDirectory: cwd,
-		// Main: main,
-		Sink: sink,
-		StatusSink: statusSink,
-	}
+    newCtx := Context{
+        // Info: projInfo,
+        PluginCtx: ctx,
+        WorkingDirectory: cwd,
+        // Main: main,
+        Sink: sink,
+        StatusSink: statusSink,
+    }
 
-	return &newCtx, nil
+    return &newCtx, nil
 }
 
 func (c *Context) Close() {
-	c.PluginCtx.Close()
+    c.PluginCtx.Close()
 }
 
 // func (c *Context) InstallPlugins() error {
-// 	return engine.RunInstallPlugins(
-// 		c.Info.Proj, c.WorkingDirectory, c.Main, nil, c.PluginCtx,
-// 	)
+//  return engine.RunInstallPlugins(
+//      c.Info.Proj, c.WorkingDirectory, c.Main, nil, c.PluginCtx,
+//  )
 // }
 
 func (c *Context) Provider(name tokens.Package, version *semver.Version) (*plugin.Provider, error) {
-	if c.providers == nil {
-		c.providers = make(map[tokens.Package]*plugin.Provider)
-	}
+    if c.providers == nil {
+        c.providers = make(map[tokens.Package]*plugin.Provider)
+    }
 
-	provider, ok := c.providers[name]
-	if !ok {
-		providerValue, err := c.PluginCtx.Host.Provider(name, version)
-		if err != nil {
-			return nil, fmt.Errorf("error getting provider: %v", err)
-		}
-		c.providers[name] = &providerValue
-		provider = &providerValue
-	}
-	return provider, nil
+    provider, ok := c.providers[name]
+    if !ok {
+        providerValue, err := c.PluginCtx.Host.Provider(name, version)
+        if err != nil {
+            return nil, fmt.Errorf("error getting provider: %v", err)
+        }
+        c.providers[name] = &providerValue
+        provider = &providerValue
+    }
+    return provider, nil
 }
 
 func (c *Context) CloseProvider(name tokens.Package) error {
-	provider, ok := c.providers[name]
-	if !ok {
-		return nil
-	}
-	if err := c.PluginCtx.Host.CloseProvider(*provider); err != nil {
-		return fmt.Errorf("error closing provider: %v", err)
-	}
-	delete(c.providers, name)
-	return nil
+    provider, ok := c.providers[name]
+    if !ok {
+        return nil
+    }
+    if err := c.PluginCtx.Host.CloseProvider(*provider); err != nil {
+        return fmt.Errorf("error closing provider: %v", err)
+    }
+    delete(c.providers, name)
+    return nil
 }
 
 func (c *Context) CloseProviders() error {
-	for key, _ := range c.providers {
-		if err := c.CloseProvider(key); err != nil {
-			return err
-		}
-	}
-	return nil
+    for key, _ := range c.providers {
+        if err := c.CloseProvider(key); err != nil {
+            return err
+        }
+    }
+    return nil
 }
 
 func (c *Context) ListPlugins() []workspace.PluginInfo {
-	return c.PluginCtx.Host.ListPlugins()
+    return c.PluginCtx.Host.ListPlugins()
 }
