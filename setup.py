d864170787d42d6a816ae7543836f3f1e0fe345c
cfeenstra67 <cameron.l.feenstra@gmail.com>
2020-10-29 01:46:39 -0400

    Revert "Bump version: 0.0.4 → 1.0.0"
    This reverts commit a5a08c7993ffdb3f123cbc6499b92dba6a5128f7.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open('requirements-dev.txt') as f:
 
 setup(
     name='pylumi',
-    version='1.0.0',
+    version='0.0.4',
     description='Python API for interacting with Pulumi resource plugins.',
     long_description=long_description,
     long_description_content_type='text/x-rst',
