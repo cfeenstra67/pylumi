32f0d7d56e06f822a938b4727bba7339913347ef
cfeenstra67 <cameron.l.feenstra@gmail.com>
2020-10-29 02:41:16 -0400

    Bump version: 0.1.0 → 0.1.1

--- pylumi/__init__.py
+++ pylumi/__init__.py
@@ -3,6 +3,6 @@ from pylumi.context import Context
 from pylumi.provider import Provider
 from pylumi.urn import URN
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __pulumi_version__ = '2.12.0'
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open('requirements-dev.txt') as f:
 
 setup(
     name='pylumi',
-    version='0.1.0',
+    version='0.1.1',
     description='Python API for interacting with Pulumi resource plugins.',
     long_description=long_description,
     long_description_content_type='text/x-rst',
