a0aee70d12378113fc04d1e532907fcdbd3de978
cfeenstra67 <cameron.l.feenstra@gmail.com>
2020-11-06 17:40:30 -0500

    Bump version: 0.1.3 → 1.0.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open('requirements-dev.txt') as f:
 
 setup(
     name='pylumi',
-    version='0.1.3',
+    version='1.0.0',
     description='Python API for interacting with Pulumi resource plugins.',
     long_description=long_description,
     long_description_content_type='text/x-rst',
