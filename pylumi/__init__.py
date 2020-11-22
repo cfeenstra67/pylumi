9708405958d0724dfa71cb43608f2be4cde1b729
cfeenstra67 <cameron.l.feenstra@gmail.com>
2020-11-22 13:41:30 -0500

    Bump version: 1.1.1 → 1.1.2

--- pylumi/__init__.py
+++ pylumi/__init__.py
@@ -12,6 +12,6 @@ from pylumi.context import Context
 from pylumi.provider import Provider
 from pylumi.urn import URN
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 __pulumi_version__ = "2.12.0"
